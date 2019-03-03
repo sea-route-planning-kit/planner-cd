e4bf605b948549eee2c5502b454313d5c04cd695
Vegard Nitter Vestad <vegard@vnvestad.com>
2019-03-04 00:15:15 +0100

    Refactor comments

--- test/test_generate.m
+++ test/test_generate.m
@@ -6,11 +6,6 @@ ship = simulator.ship.load('ship_viknes830.json');
 %% Calculate
 result = cd.generate(ship, scenario);
 
-%% Plot performance
-%clf
-%per = performance.calculate(ship, scenario, result.path);
-%performance.compare(scenario, [per], {'CD'});
-
 %% Plot method results
 figure(3);
 clf
